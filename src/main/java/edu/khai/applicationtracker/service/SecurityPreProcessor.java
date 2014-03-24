afab8963d4f76a8a7458d2d0f42ce2eb44d13867
angry-quester <roman_quester@mail.ru>
2014-03-24 09:55:08 +0300

    Deleted unused interface

--- src/main/java/edu/khai/applicationtracker/service/SecurityPreProcessor.java
+++
@@ -1,6 +0,0 @@
-package edu.khai.applicationtracker.service;
-
-
-public interface SecurityPreProcessor {
-	public boolean preProcess(SecuredService securedService, Long principalId, Long entityId);
-}
